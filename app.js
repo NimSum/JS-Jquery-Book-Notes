c9d66b3eeb965e18979b2c91c3a34d27c6a7411e
NimSum <nim.garcia@yahoo.com>
2018-06-29 01:55:50 -0400

    from previous to pg. 95

--- app.js
+++ app.js
@@ -69,13 +69,13 @@ console.log(shipping);
 //page 71 Array Examples
 //Array literal is preferred(one with [])
 var colors;
-colors = ['white', 'blue', 'green'];
+colors = ['white', 'blue', 'green', 'custom'];
 // OR Array constructor below
 // var colors = new Array ('white',
 // 						'blue',
 // 						'green');
 var laColors = document.getElementById('colors');
-laColors.textContent = colors[2];
+laColors.textContent = colors[1];
 //accessing an array
 var theColorGreen = colors[2]
 console.log(theColorGreen);
@@ -83,8 +83,59 @@ console.log(theColorGreen);
 var colorAmount = colors.length;
 console.log(colorAmount);
 //changing/updating array values
-colors[1] = 'red';
-console.log(colors[1]);
+colors[3] = 'yellow'; //can create a new value with any number you want
+console.log(colors[3]); // ie colors[20] even if it's not in the current array yet
+//Page 74 EXPRESSIONS - 2 Types
+//type 1 ONLY a value is assigned
+var color = 'black';
+//type 2 uses TWO or more values to give return a single value
+var multiply = 2 * 6;
+//var length = '2'; // cannot use arithmetic operation if numeric value is inside ''
+console.log('Two times Six: ' + multiply)
+
+//combining strings using + operator(concatenating)
+var robotName = ' Butterbots ';
+var purpose = 'purpose is to pass butter ';
+var times = 12 + ' times.';
+var robotsPurpose = robotName + purpose + times;
+
+var elRobot = document.getElementById('concatenation');
+elRobot.textContent = robotsPurpose;
+
+//combining arithmetic operators
+var length = 10;
+var width = 5;
+var area = length * width;
+
+var elArea = document.getElementById('areaCalc');
+elArea.textContent = ' Length * Width is equal to area: ' + area;
+
+var elLength = document.getElementById('theLength');
+elLength.textContent = ' The length is ' + length;
+
+var elWidth = document.getElementById('theWidth');
+elWidth.textContent = ' The Width is ' + width;
+
+///Basic Functions, Methods and Objects
+var newMessage = 'You see this instead of what is written insde the html tag.';
+function updateNewMessage() {
+	var elMsg = document.getElementById('message');
+	elMsg.textContent = newMessage;
+}
+updateNewMessage();
+//function that requires parameters
+function areaCalc(aLength, aWidth) {
+	return aLength * aWidth;
+}
+var calculatedArea = areaCalc(length, width);
+console.log('Used w/ basic variable: ' + calculatedArea);
+//can be re-used with different values
+var exArray = [5, 10, 15, 20]
+var otherArea = areaCalc(exArray[0], exArray[3]);
+console.log('Used w/ an Array: ' + otherArea);
+
+
+
 
 
 
