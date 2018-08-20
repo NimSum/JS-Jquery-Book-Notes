a9a9e48e4151709fc54984ed7308043f0848a8af
NimSum <nim.garcia@yahoo.com>
2018-08-19 20:51:57 -0400

    not alot of progress

--- eventsCH6.js
+++ eventsCH6.js
@@ -38,5 +38,88 @@ function checkParameterLengthThree(totalLength) {
 	}
 }
 parameterTarget.addEventListener('blur', function() {//anonymous function
-	checkParameterLengthThree(6);//will not execute right away because its not in an anonymous function
+	checkParameterLengthThree(5);//will not execute right away because its not in an anonymous function
 }, false);
+
+
+//EVENT DELEGATION EXAMPLE AINT WORKINGG!
+// function getTarget(e) {
+// 	if (!e) {
+// 		e = window.event;
+// 	}
+// 	return e.target || e.srcElement;
+// }
+
+// function itemDone(e) {
+// 	var target, elParent, elGrandparent;
+// 	target = getTarget(e);
+// 	elParent = target.parentNode;
+// 	elGrandParent = target.parentNode.parentNode;
+// 	elGrandparent.removeChild(elParent);
+
+// 	if (e.preventDefault) {
+// 		e.preventDefault(); 
+// 	} else {
+// 		e.returnValue = false;
+// 	}
+// }
+
+// var el = document.getElementById('shoppingList');
+// if (el.addEventListener) {
+// 	el.addEventListener('click', function(e) {
+// 		itemDone(e);
+// 	}, false);
+// } else {
+// 	el.attachEvent('onclick', function(e) {
+// 		itemDone(e);
+// 	});
+// }
+
+
+//FOCUS AND BLUR
+function checkUserName () {
+	var username = el.value;//set value to input of user
+	if (username.length < 5) {//condition requiredment
+		elMsg.className = 'eventHandler'//set css class for warning message
+		elMsg.textContent = 'Not long enough bro, common...';//warning message
+	} else {
+		el.Msg.textContent = '';//if criteria met, no message needed
+	}
+}
+function tipUsername() {
+	elMsg.className = 'eventHandlerChild';//set tip to a specific class
+	elMsg.textContent = '5 Chars or more please';//tip text 
+}
+
+var el = document.getElementById('focusAndBlurInput');//set targets
+var elMsg = document.getElementById('focusAndBlurFeedBack');//set targets
+
+el.addEventListener('focus', tipUsername, false);//listen for focus event
+el.addEventListener('blur', checkUserName, false);//listen for blur event
+
+
+//MOUSE EVENTS
+var msg = 'div class=\"header\"><a id=\"close\" href "#">close X</a></div>';
+msg += '<div><h2>YOU SHOULD NOT HAVE DONE THAT</h2>';
+msg += 'Now you will pay the price!';
+msg += 'The price of clicking that exit button on the top right! Muahahaha.</div>';
+
+// var warningMessage = document.createElement('div');
+// warningMessage.setAttribute('id', 'note');
+// warningMessage.innerHTML = msg;
+// document.body.appendChild('warningMessage');
+
+// function dismissWarning() {
+// 	document.body.removeChild(warningMessage);
+// }
+
+// var closeWarning = document.getElementById('button');
+// closeWarning.addEventListener('click', dismissWarning, false);
+
+function warningMessage() {
+	var warningPopUp = document.createElement('div');
+	warningPopUp.innerHTML = msg;
+}
+var buttonClick = document.getElementById('button');
+buttonClick.addEventListener('click', warningMessage, false);
+
